caebc6b4e62c5af80aced800f685ca6b8e783e6d
HAWKEYE <hawkeye@ovoll.in>
2026-09-21 13:42:29 +0530

    feat(branding): replace default Laravel favicon with bespoke ELIOR architectural monogram

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,7 +9,9 @@
 
         <link rel="icon" href="/favicon.ico" sizes="any">
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
         <meta name="theme-color" content="#0F0F0F">
         <meta name="msapplication-TileColor" content="#0F0F0F">
 
